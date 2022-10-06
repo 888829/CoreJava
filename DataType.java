28e824a5a0271598a92c3cf760f49c1cf1151a16
888829 <115135110+888829@users.noreply.github.com>
2022-10-06 12:24:14 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,24 @@
+package datatype;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Boolean flag=true;
+byte range=24;
+short temp=-200;
+int range1=43760000;
+long range2=156886516596558l;
+float f1=3.17f;
+double f2=32126596.225;
+System.out.println(flag);
+System.out.println(range);
+System.out.println(temp);
+System.out.println(range1);
+System.out.println(range2);
+System.out.println(f1);
+System.out.println(f2);
+
+	}
+
+}
